6e2f2097ee72768f495c7ecff527675ab2a3111b
Leon <wonderboy0609@outlook.com>
2022-03-29 20:24:15 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        'widget/**/*.svelte',
+        'widget/components/Comment.svelte',
+        'widget/components/Reply.svelte',
+        'widget/theme.css',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
